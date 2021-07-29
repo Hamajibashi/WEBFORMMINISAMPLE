19c8dbb83f1d71ad28fd7f21d109b59aba4b92d9
mylenewlwang@gmail.com <mylenewlwang@gmail.com>
2021-07-29 15:05:53 +0800

    加入專案

---
+++ WebFormMiniSample/AccountingNote.DBSource/UserInfoManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingNote.DBSource
+{
+    public class UserInfoManager
+    {
+        private static string GetConnctionString()
+        {
+            //string val = ConfigurationManager.AppSettings["ConnectionString"];
+            //return val; //取得連線字串
+
+            string val = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            return val;
+        }
+
+        public static DataRow GetUserInfoByAccount(string account)
+        {
+            string connectionString = GetConnctionString();
+
+            string dbCommandString = @"SELECT [ID], [Account], [PWD], [Name], [Email] FROM UserInfo WHERE [Account] = @account";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(dbCommandString, connection))
+                {
+                    command.Parameters.AddWithValue("@account", account);
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        reader.Close();
+
+                        if (dt.Rows.Count == 0)
+                            return null;
+
+                        DataRow dr = dt.Rows[0];
+                        return dr; //改回傳值，並移除輸出
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.WriteLog(ex);
+                        return null; //改回傳值
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ WebFormMiniSample/AccountingNote/Login.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="AccountingNote.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:PlaceHolder runat="server" ID="plclogin" Visible="false">
+        Account: <asp:TextBox ID="txtAccount" runat="server"></asp:TextBox>
+        Password: <asp:TextBox ID="txtPWD" runat="server" TextMode="Password"></asp:TextBox>
+        <p>
+        <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+        </p>
+        <p>
+        <asp:Literal ID="ltltMsg" runat="server"></asp:Literal>
+        </p>
+        </asp:PlaceHolder>
+    </form>
+</body>
+</html>
